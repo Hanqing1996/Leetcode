dabfcceaaf1172ebfa695c3ba8708e1e5de8a2ed
朱汉卿 <zhq199622@163.com>
2019-06-04 09:58:51 +0800

    Create DP-L121.java

---
+++ DP-L121.java
@@ -0,0 +1,14 @@
+/*
+ dp[i]表示以price[i]价格出售的可获最大利润
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[] dp=new int[100];
+        dp[0]=0;//第一天卖出，利润为0
+        for(int i=1;i<n;i++)
+        {
+            dp[i]=d
+        }
+        
+    }
+}
