33d34256bbba5e19f1418248417698bccbcd8ca1
朱汉卿 <zhq199622@163.com>
2019-06-05 09:37:26 +0800

    Update DP-L121.java

--- DP-L121.java
+++ DP-L121.java
@@ -3,12 +3,17 @@
  */
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] dp=new int[100];
-        dp[0]=0;//第一天卖出，利润为0
-        for(int i=1;i<n;i++)
+        int[] dp=new int[100001];
+        int result=0;
+        for(int i=0;i<prices.length;i++)
         {
-            dp[i]=d
+            dp[i]=0;//初始值为0
+            for(int j=0;j<i;j++)
+            {
+                dp[i]=Math.max(dp[i],prices[i]-prices[j]);
+                result=Math.max(result,dp[i]);
+            }
         }
-        
+        return result;
     }
-}
+} 
