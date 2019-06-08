3da9c4a2e147572bb12e78fb1459c369972a7a7d
朱汉卿 <zhq199622@163.com>
2019-06-08 10:27:15 +0800

    Update 3. 无重复字符的最长子串.cpp

--- 3. 无重复字符的最长子串.cpp
+++ 3. 无重复字符的最长子串.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int dp[10010];
+        int dp[10010];//dp[i]表示以s[i]结尾的最长子串长度
         dp[0]=1;
         string cur="";
         cur+=s[0];
