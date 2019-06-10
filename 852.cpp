f67a5d2322bbe6e24ac876beab3805b4fed3c5e7
朱汉卿 <zhq199622@163.com>
2019-06-10 10:13:49 +0800

    Update 852.cpp

--- 852.cpp
+++ 852.cpp
@@ -18,20 +18,23 @@ public:
 };
 
 /*
- * 思路2：直接排序，最大元素下标即为答案，复杂度为O(n)
+ * 思路2：二分查找，复杂度为O(logn)
  */
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int Max=-1,res;
-        for(int i=0;i<A.size();i++)
+        int low=0,high=A.size()-1,mid; 
+        while(low<=high)
         {
-            if(A[i]>Max) 
-            {
-                Max=A[i];
-                res=i;
-            }
+            mid=(low+high)/2;
+            //注意A数组只有一个元素满足相邻元素皆小于它，即所求关键字
+            if(A[mid]>A[mid-1]&&A[mid]<A[mid+1])
+                break;
+            else if(A[mid]<A[mid-1])
+                high=mid;
+            else 
+                low=mid;
         }
-        return res;
+        return mid;
     }
 };
