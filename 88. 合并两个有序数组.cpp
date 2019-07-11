385453f633acd6713356a9fbd465ff3e84033dd3
朱汉卿 <zhq199622@163.com>
2019-07-11 16:57:37 +0800

    Create 88. 合并两个有序数组.cpp

---
+++ 88. 合并两个有序数组.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        //事先存储nums1的m个元素是有必要的，比如num1=[9,10],num2=[1,2,3,4]时
+        vector<int> nums3=nums1;
+        
+        int index1=0,index2=0,count=0;
+        while(index1<m&&index2<n)
+        {
+            if(nums3[index1]<nums2[index1]){
+                nums1[count++]=nums3[index1];
+                index1++;
+                count++;
+            }
+            else{
+                nums1[count++]=nums2[index2];
+                index2++;
+            }
+        }
+        if(index1<m){
+            for(int i=count;i<m+n;i++)
+            {
+                nums1[i]=nums3[i-count];
+            }
+        }
+        else{
+            for(int i=count;i<m+n;i++)
+            {
+                nums1[i]=nums2[i-count];
+            }
+        }
+        
+    }
+};
