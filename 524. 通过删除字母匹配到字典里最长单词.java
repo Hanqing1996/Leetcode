06b9640762650db71668a63770c8ad416182a6c4
朱汉卿 <zhq199622@163.com>
2019-07-16 10:27:08 +0800

    Create   524. 通过删除字母匹配到字典里最长单词.java

---
+++ 524. 通过删除字母匹配到字典里最长单词.java
@@ -0,0 +1,12 @@
+
+//判断能否通过删除s的部分字符得到target
+private boolean isSubstr(String s, String target) {
+    int i = 0, j = 0;
+    while (i < s.length() && j < target.length()) {
+        if (s.charAt(i) == target.charAt(j)) {
+            j++;
+        }
+        i++;
+    }
+    return j == target.length();
+}
