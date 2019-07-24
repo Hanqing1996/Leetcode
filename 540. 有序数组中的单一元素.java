1cc23f2c8f1ec56da42ed67cfaea494f3b2fad52
朱汉卿 <zhq199622@163.com>
2019-07-24 19:56:22 +0800

    Update 540. 有序数组中的单一元素.java

--- 540. 有序数组中的单一元素.java
+++ 540. 有序数组中的单一元素.java
@@ -1,31 +1,34 @@
 class Solution {
-    
-    // 要找的元素在数组中下标必然为偶数
-    public int singleNonDuplicate(int[] nums) {
-        int left=0,right=nums.length-1,mid=0,sum;//注意nums.length必然为奇数
-        while(left<right)
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left=0,right=nums.size()-1,mid=0,sum;// 注意nums.length必然为奇数
+        while(left<=right)
         {
-            sum=left+right;
-            if(sum%2==0)
-                mid=sum/2;
-            else
+            // mid可能为奇数,可能为偶数
+            mid=(left+right)/2;
+            
+            // 偶数处理
+            if(mid%2==0)
             {
-                if(nums[mid]==nums[mid-1])
-                    mid=mid+1;  
-                else if(nums[mid]==nums[mid+1])
-                    mid=mid-1;
+                if(nums[mid]==nums[mid-1]) // 左等,说明key在左边
+                    right=mid-2;   
+                else if(nums[mid]==nums[mid+1]) // 右等,说明key在右边
+                    left=mid+2;
+                else
+                    break; //找到key
+            }
+            
+            // 奇数处理
+            if(mid%2==1)
+            {
+                if(nums[mid]==nums[mid-1]) // 左等,说明key在右边
+                    left=mid+1;
+                else if(nums[mid]==nums[mid+1]) // 右等,说明key在左边
+                    right=mid-1;
                 else
                     break;
             }
-                
-            if(nums[mid]==nums[mid-1])
-                right=mid-2;
-            else if(nums[mid]==nums[mid+1])
-                left=mid+2;
-            else
-                break;
-            
         }
-        return nums[mid];
+         return nums[mid];
     }
-}
+};
