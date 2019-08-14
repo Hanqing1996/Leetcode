d7db6e84de1b202251fb851546dabef3b99a06ee
朱汉卿 <zhq199622@163.com>
2019-08-14 09:21:20 +0800

    Create 328. 奇偶链表.java

---
+++ 328. 奇偶链表.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        // 链表长度为0,1,2;直接返回head
+        if(head==null||head.next==null||head.next.next==null)
+            return head;
+        
+        ListNode odd=head,even=head.next;
+        
+        
+        // 保证链表长度大于等于3.链表长度为奇数:even=null时不做处理,链表长度为偶数:even.next=null时不做处理
+        while(even!=null&&even.next!=null){
+            
+            // 删除奇数节点
+            ListNode nextTmp=even.next;
+            even.next=even.next.next;
+            
+            // 插入奇数节点
+            nextTmp.next=odd.next;
+            odd.next=nextTmp;
+            
+            // 移动even和odd
+            even=even.next;
+            odd=odd.next;
+        }
+        return head;
+    }
+} 
