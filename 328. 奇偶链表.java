d80430841190a4b89fb50f83da4b8ed07409e5ca
朱汉卿 <zhq199622@163.com>
2019-08-19 09:23:36 +0800

    Update 328. 奇偶链表.java

--- 328. 奇偶链表.java
+++ 328. 奇偶链表.java
@@ -6,6 +6,10 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/**
+ * 迭代
+ */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         
@@ -32,5 +36,38 @@ class Solution {
             odd=odd.next;
         }
         return head;
+    }
+} 
+
+/**
+ * 递归
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        // 链表长度为0,1,2;直接返回head
+        if(head==null||head.next==null||head.next.next==null)
+            return head;
+        
+        ListNode odd=head,even=head.next;
+        
+        return oddEven(even,odd,head);
+    }
+
+    
+    private ListNode oddEven(ListNode even,ListNode odd,ListNode head){
+
+        if(even==null||even.next==null)
+        return head;
+
+        // 删除奇数节点
+        ListNode nextTmp=even.next;
+        even.next=even.next.next;
+        
+        // 插入奇数节点
+        nextTmp.next=odd.next;
+        odd.next=nextTmp;
+
+        return oddEven(even.next,odd.next,head);
     }
 } 
