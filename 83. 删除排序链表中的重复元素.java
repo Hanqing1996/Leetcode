1a6408ef4e7fe5aa66661c423625a344b2f21cbd
朱汉卿 <zhq199622@163.com>
2019-08-19 09:10:32 +0800

    Update 83. 删除排序链表中的重复元素.java

--- 83. 删除排序链表中的重复元素.java
+++ 83. 删除排序链表中的重复元素.java
@@ -6,6 +6,10 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/**
+ * 迭代
+ */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         
@@ -30,5 +34,36 @@ class Solution {
                 cur=cur.next;   
         }
         return head;
+    }
+}
+
+/**
+ * 递归
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode cur=head;
+        
+        // 链表为空
+        if(cur==null)
+            return head;
+        
+        return delete(cur,cur.next,head);
+    }
+
+    private ListNode delete(ListNode cur,ListNode nextTmp,ListNode head){
+        if(nextTmp==null)
+        return head;
+
+        // 节点删除
+        if(cur.val == nextTmp.val){
+            cur.next=nextTmp.next; // 注意nextTmp.next容许为空
+            nextTmp.next=null;
+        }
+        else
+            cur=cur.next; 
+
+        return delete(cur,cur.next,head);    
     }
 }
