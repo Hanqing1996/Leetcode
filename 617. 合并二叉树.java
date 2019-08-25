899e6199e6fcb7f0f1c7afde20d9faf2f8fa12e9
朱汉卿 <zhq199622@163.com>
2019-08-25 10:03:56 +0800

    Create 617. 合并二叉树.java

---
+++ 617. 合并二叉树.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        
+        if(t1!=null&&t2!=null){
+            t1.val+=t2.val;
+            
+            t1.left=merge(t1.left,t2.left);
+            t1.right=merge(t1.right,t2.right);
+
+            return t1; // 这一句会多次执行
+        }
+        else if(t1==null&&t2!=null){
+            return t2;
+        }
+        else
+            return t1;
+    }
+    
+}
