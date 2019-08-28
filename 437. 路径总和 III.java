6bb7b64134759460bdad0d616eb1e41df52f5ca5
朱汉卿 <zhq199622@163.com>
2019-08-28 09:06:14 +0800

    Create 437. 路径总和 III.java

---
+++ 437. 路径总和 III.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public int pathCount=0,requireSum=0;
+    public int pathSum(TreeNode root, int sum) {
+        
+        requireSum=sum;
+        
+        preOrder(root);
+        
+        return pathCount;
+    }
+    
+    // 搜索每个节点到其子节点的所有路径，若存在路径和为sum，则pathCount++
+    private void path(TreeNode root, int currentSum){
+        
+        if(root!=null){
+            
+            if(currentSum+root.val==requireSum)
+                pathCount++;
+            
+            path(root.left,currentSum+root.val);
+            path(root.right,currentSum+root.val);
+                
+        }
+    }
+    
+    // 由于一次遍历总是只能计算以根结点开始的累计和，因此需要进行两次遍历以保证所有节点都做过根节点
+    private void preOrder(TreeNode root){
+        
+         if(root!=null){
+            
+            path(root,0);
+            preOrder(root.left);
+            preOrder(root.right); 
+        }
+    }
+ 
+}
