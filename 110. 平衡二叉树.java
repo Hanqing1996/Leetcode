acd83b03cd58739a0e1d7469a570dccde7673d8b
朱汉卿 <zhq199622@163.com>
2019-09-11 09:23:12 +0800

    Update 110. 平衡二叉树.java

--- 110. 平衡二叉树.java
+++ 110. 平衡二叉树.java
@@ -34,3 +34,53 @@ class Solution {
             return 0;
     }   
 }
+
+
+/**
+ * 二次遍历
+ */
+class Solution {
+    
+    public int currentDepth=0;
+    public boolean isBalance=true;
+    
+    public boolean isBalanced(TreeNode root) {
+        
+        preorder(root);
+        
+        return isBalance;
+    }
+    
+    private void preorder(TreeNode root){
+        if(root!=null){
+            
+            // 左子树高度
+            currentDepth=0;
+            getDepth(root.left,0);
+            int leftDepth=currentDepth;
+            
+            // 右子树高度
+            currentDepth=0;
+            getDepth(root.right,0);
+            int rightDepth=currentDepth;
+            
+            // 左右子树高度相差超过1,判定为不是平衡二叉树
+            if(Math.abs(leftDepth-rightDepth)>1)
+                isBalance=false;
+            
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    
+    private void getDepth(TreeNode root,int depth){
+        if(root!=null)
+        {
+            depth+=1;
+            currentDepth=Math.max(currentDepth,depth);
+            
+            getDepth(root.left,depth);
+            getDepth(root.right,depth);
+        }
+    }
+}
