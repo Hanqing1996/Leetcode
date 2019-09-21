9770d274b15018f96e44329d622c352b04bf203b
朱汉卿 <zhq199622@163.com>
2019-09-21 09:03:41 +0800

    Create 337. 打家劫舍 III.cpp

---
+++ 337. 打家劫舍 III.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * 两次遍历行不通
+ * 因为不一定根节点不选，两个子节点就一定都会入选,比如[2,7,1,null,null,null,7],最大收益序列为[7,7]
+ */
+class Solution {
+    
+    public int rob(TreeNode root) {
+        
+    }
+}
