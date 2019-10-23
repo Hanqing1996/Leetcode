04bbf1bd310e7a28fb0760cd25f6228d22e471b6
朱汉卿 <zhq199622@163.com>
2019-10-23 13:27:53 +0800

    Create 1189. “气球” 的最大数量.cpp

---
+++ 1189. “气球” 的最大数量.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int Hash[256];
+        string target="balloon";
+        
+        for(int i=0;i<text.size();i++){
+            Hash[text[i]-'a']++;
+        }
+        
+        Hash['l'-'a']=Hash['l'-'a']/2;
+        Hash['o'-'a']=Hash['o'-'a']/2;
+        
+        int minLen=10000;
+        for(int i=0;i<target.size();i++){
+            minLen=min(minLen,Hash[target[i]-'a']);
+        }
+        
+        if(minLen==10000)
+            return 0;
+        return minLen;
+    }
+};
