813645efc21927f60d47251717db9641a1e217e9
朱汉卿 <zhq199622@163.com>
2019-10-25 08:48:20 +0800

    Update 645. 错误的集合.cpp

--- 645. 错误的集合.cpp
+++ 645. 错误的集合.cpp
@@ -2,18 +2,27 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(), nums.end());
+        set<int> se;
         vector<int> res;
         int Hash[10001];
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            if(Hash[nums[i]]==1){
-                res.push_back(nums[i]);
-            }
-            Hash[nums[i]]++;
-            sum+=nums[i];
+        int numsSum=0,seSum=0;
+        for(int i=0;i<n;i++){
+            se.insert(nums[i]);
+            numsSum+=nums[i];
         }
-        res.push_back(res[0]-(sum-(1+n)*n/2));
+        
+        // 找重复出现的数字
+        for(auto it = se.begin(); it != se.end(); it++){
+            seSum+=*it;
+        }
+        res.push_back(numsSum-seSum);
+        
+        // 找缺失的数字
+        for(int i=1;i<=n;i++){
+            if(se.find(i)== se.end())
+                res.push_back(i);
+        }
+
         return res;
     }
 };
