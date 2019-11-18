fd17b332dc4682af6cd1f40e155c60959a2fedcc
朱汉卿 <zhq199622@163.com>
2019-11-18 19:31:54 +0800

    Update 88. 合并两个有序数组.cpp

--- 88. 合并两个有序数组.cpp
+++ 88. 合并两个有序数组.cpp
@@ -33,3 +33,19 @@ public:
         
     }
 };
+
+
+// 更飘逸的写法
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+        
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+    }
+};
