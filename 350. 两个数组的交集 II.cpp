e81ad8ed91319dbc50bd4492d1abf207a4e84c1a
朱汉卿 <zhq199622@163.com>
2019-11-19 19:15:59 +0800

    Update 350. 两个数组的交集 II.cpp

--- 350. 两个数组的交集 II.cpp
+++ 350. 两个数组的交集 II.cpp
@@ -1,30 +1,19 @@
-/*
- * 用Hash+set
- */
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        int Hash1[10010],Hash2[10010];
-        set<int> s1,s2;
+        map<int,int> Hash;
+        int len1=nums1.size();
+        int len2=nums2.size();
+        for(int i=0;i<len2;i++){
+            Hash[nums2[i]]++;
+        }
         vector<int> res;
-        for(int i=0;i<nums1.size();i++)
-        {
-            Hash1[nums1[i]]++;
-            s1.insert(nums1[i]);
-        }
-        for(int i=0;i<nums2.size();i++)
-        {
-            Hash2[nums2[i]]++;
-            s2.insert(nums2[i]);
-        }
-        for(auto it=s1.begin();it!=s1.end();it++)
-        {
-            if(s2.find(*it)!=s2.end())
-            {
-                for(int i=0;i<min(Hash1[*it],Hash2[*it]);i++)
-                {
-                    res.push_back(*it);
-                }
+        for(int i=0;i<len1;i++){
+            // Hash[nums1[i]]>0,说明nums1[i]一定重复了
+            if(Hash[nums1[i]]>0){
+                res.push_back(nums1[i]);
+                // 消耗掉一个nums2里面的一个重复元素
+                Hash[nums1[i]]--;
             }
         }
         return res;
