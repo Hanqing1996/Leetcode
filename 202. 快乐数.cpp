cc4bd4fa6174e725c1c2d708aaab09c9cc4f9f77
朱汉卿 <zhq199622@163.com>
2019-11-19 10:24:46 +0800

    Create 202. 快乐数.cpp

---
+++ 202. 快乐数.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // 本题关键在于“无限循环”不好观测
+    // 用快慢指针判断一次循环是否结束(等价于一直检测是否有重复数出现)
+    int bitSquareSum(int n) {
+        int sum = 0;
+        while(n > 0)
+        {
+            int bit = n % 10;
+            sum += bit * bit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow = bitSquareSum(slow);
+            fast = bitSquareSum(fast);
+            fast = bitSquareSum(fast);
+        }while(slow != fast);
+        
+        return slow == 1;
+    }
+};
