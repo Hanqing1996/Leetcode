ec8a125a389bdfb962300503f2cbb21d1c567133
朱汉卿 <zhq199622@163.com>
2020-10-29 20:42:23 +0800

    Update 442. 数组中重复的数据【原地hash】.js

--- js/442. 数组中重复的数据【原地hash】.js
+++ js/442. 数组中重复的数据【原地hash】.js
@@ -16,9 +16,9 @@
         i++;
     }
 
-    // 在本题限定条件下，只要发现原地哈希后，nums[nums[i]-1]!=i+1，就证明nums[i]重复了
+    // 在本题限定条件下，只要发现原地哈希后，nums[i]!=i+1，就证明nums[i]重复了
     for(let i=0;i<nums.length;i++){
-        if(nums[nums[i]-1]!=i+1){
+        if(nums[i]!=i+1){
             result.push(nums[i])
         }
     }
