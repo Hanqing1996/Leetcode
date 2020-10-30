426fa5c9f1757b0a16a5650bb2c4329beaeca8bc
朱汉卿 <zhq199622@163.com>
2020-10-30 10:14:25 +0800

    Update 27. 移除元素.js

--- js/27. 移除元素.js
+++ js/27. 移除元素.js
@@ -6,13 +6,13 @@
 
  // 双指针，p指向不等于val的元素，q用于遍历 nums 数组（注意 p,q不是同步增长的，我们需要用 q来填充 nums[p]）
 var removeElement = function(nums, val) {
-    let i=0
-    for(let j=0;j<nums.length;j++){
-        if(nums[j]!=val){
-            nums[i]=nums[j]
-            i++
+    let p=0
+    for(let q=0;q<nums.length;q++){
+        if(nums[q]!=val){
+            nums[p]=nums[q]
+            p++
         }
     }
-    // 数组已经遍历完，此时i即为原nums中不等于val的元素个数
-    return i
+    // 数组已经遍历完，此时p即为原nums中不等于val的元素个数
+    return p
 };
