738d832cbb53c82216e7e9792355931d797a6c62
朱汉卿 <zhq199622@163.com>
2020-11-03 19:46:37 +0800

    Update 47. 全排列 II【回溯】.js

--- js/47. 全排列 II【回溯】.js
+++ js/47. 全排列 II【回溯】.js
@@ -8,7 +8,7 @@ var permuteUnique = function(nums) {
     nums=nums.sort((a,b)=>a-b)
 
     let ans=[],res=[]
-    let visited=Array(1024).fill(false) 
+    let visited=Array(nums.length).fill(false) 
 
     let DFS=function(depth){
         if(depth===nums.length){
@@ -18,9 +18,10 @@ var permuteUnique = function(nums) {
 
         for(let i=0;i<nums.length;i++){
             // [...1,1...] 如果上次已经设置 ans[i-1]=1(前面那个)，本次 ans[i]=1(后面那个)就不能设置了，否则会重复
-            if(i>0&&nums[i]===nums[i-1]&&!visited[i-1]&&!visited[i])
-                continue
+            // !visited[i-1] 是用来保证目前是对纵向遍历作拦截(!visited[i-1] 证明一定是刚刚从下一个 DFS 中回退回来)
             if(!visited[i]){
+                if(i>0&&nums[i]===nums[i-1]&&!visited[i-1])
+                    continue
                 ans[depth]=nums[i]
                 visited[i]=true
                 DFS(depth+1)
