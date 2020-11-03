21f904424073713e5bbf7db3ea52affa03d6b514
朱汉卿 <zhq199622@163.com>
2020-11-03 19:46:12 +0800

    Update 39. 组合总和【回溯】.js

--- js/39. 组合总和【回溯】.js
+++ js/39. 组合总和【回溯】.js
@@ -1,21 +1,23 @@
 const combinationSum = (candidates, target) => {
-    const res = [];
-    const dfs = (start, temp, sum) => {
+    let ans=[]
+    let res = [];
+    const dfs = (index, count, sum) => {
       if (sum >= target) {
         if (sum == target) {
-          res.push([...temp]); // temp的拷贝
+          res.push([...ans.slice(0,count)]);
         }
-        return;     // 结束当前递归
+        return; 
       }
-      for (let i = start; i < candidates.length; i++) { // 枚举出选择，从start开始
-        // 无论是背包还是回溯，剪枝条件是一样的
+      for (let i = index; i < candidates.length; i++) {
         if(sum+candidates[i]<=target){
-            temp.push(candidates[i]);           // 加入“部分解”
-            dfs(i, temp, sum + candidates[i]);  // 往下继续选择，同时sum累加上当前数字
-            temp.pop();                         // 撤销选择
+            ans[count]=candidates[i]         
+            dfs(i, count+1, sum + candidates[i]);
         }
       }
     };
-    dfs(0, [], 0);
+    dfs(0, 0, 0);
     return res;
   };
+
+ // 横向:[2->2->2->2]的递进是 DFS不断深入到下层+ 过滤 index 之前的数字（但是不像 46 题那样设置了 visited 过滤）
+ // 纵向:[2,2,2->3 ...]的递进是DFS回退到上层（DFS(2)->DFS(1)）后继续之前的for循环，for 循环遍历的结果（DFS 回退后 i++,所以nums由2遍历到3） 
