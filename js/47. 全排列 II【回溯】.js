54ae6c766c2b3d42758ea234db2ccefc377502c8
朱汉卿 <zhq199622@163.com>
2020-11-03 13:12:13 +0800

    Create 47. 全排列 II【回溯】.js

---
+++ js/47. 全排列 II【回溯】.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+
+    // 排序，把相同的元素聚集起来，以方便后面的重复过滤
+    nums=nums.sort((a,b)=>a-b)
+
+    let ans=[],res=[]
+    let visited=Array(1024).fill(false) 
+
+    let DFS=function(depth){
+        if(depth===nums.length){
+            res.push([...ans])
+            return 
+        }
+
+        for(let i=0;i<nums.length;i++){
+            // [...1,1...] 如果上次已经设置 ans[i-1]=1(前面那个)，本次 ans[i]=1(后面那个)就不能设置了，否则会重复
+            if(i>0&&nums[i]===nums[i-1]&&!visited[i-1]&&!visited[i])
+                continue
+            if(!visited[i]){
+                ans[depth]=nums[i]
+                visited[i]=true
+                DFS(depth+1)
+                visited[i]=false
+            }
+        }  
+    }
+
+    DFS(0)
+    return res
+};
