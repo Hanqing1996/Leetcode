3a6afb401d20540da5158a4e225873b691fefc12
朱汉卿 <zhq199622@163.com>
2020-12-12 10:20:45 +0800

    Update quickSort.js

--- js/排序算法/quickSort.js
+++ js/排序算法/quickSort.js
@@ -3,11 +3,11 @@ const arr = [30, 32, 6, 24, 37, 32, 45, 21, 38, 23, 47];
 // 好粗暴，不过我喜欢
 function quickSort(arr){
 
-  let temp = arr[0];
+  let pivot = arr[0];
   let left = [];
   let right = [];
   for(let i = 1; i < arr.length; i++){
-    if(arr[i] > temp){
+    if(arr[i] > pivot){
       right.push(arr[i]);
     }else{
       left.push(arr[i]);
