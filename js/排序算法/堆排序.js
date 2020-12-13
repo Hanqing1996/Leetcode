2df835c0e9359a71ef2b7fa8873b96afee9e312a
朱汉卿 <zhq199622@163.com>
2020-12-13 15:43:15 +0800

    Update 堆排序.js

--- js/排序算法/堆排序.js
+++ js/排序算法/堆排序.js
@@ -1,55 +1,56 @@
+
+function swap(a,b,arr){
+    let temp=arr[a]
+    arr[a]=arr[b]
+    arr[b]=temp
+}
+
 function heapSort(arr){
-    let result=[] 
+
+    arr=[,...arr]
 
     // 建立一个大顶堆
-    let heap=buildHeap(arr)
-    let len=heap.length-1
+    buildHeap(arr)
+
+    console.log(arr)
+
+    let len=arr.length-1
+
+    let result=[]
+
+    // 总共要取len个元素放入 result
     for(let i=0;i<len;i++){
-        [heap[1],heap[len-i]]=[heap[len-i],heap[1]]
-        //swap(heap,1,len-i)
-        result.unshift(heap.pop())
-        heap=buildHeap(heap)
+        swap(1,len-i,arr)
+        result.unshift(arr.pop())
+        buildHeap(arr)
     }
     return result
 }
 
-
-
-
 function buildHeap(arr){
-    // 从最后一个非叶子节点开始,注意 arr[0]不用于存储数据，即arr对应完全二叉树的映射下标是直接从1开始的
     let heapSize=arr.length-1
     for(let i=Math.floor(heapSize/2);i>=1;i--){
-        heapifyBig(arr,i,heapSize)
+        heapifyBig(i,arr,heapSize)
     }
-    return arr
 }
 
-// 建立大顶堆
-function heapifyBig(arr,i,heapSize){
-    // 让人惊艳的while循环，保证了在以i节点为父节点时若发生父子节点值呼唤，则会自动向i节点以后检验子节点树是否符合大顶堆特性
-    while (true) {
-        var maxIndex = i;
-        if(2*i <= heapSize && arr[i] < arr[i*2] ) {
-            maxIndex = i*2;
+function heapifyBig(i,arr,heapSize){
+    while(true){
+        let maxIndex=i
+        if(i*2<=heapSize&&arr[i*2]>arr[i]){
+            maxIndex=i*2
         }
-        if(2*i+1 <= heapSize && arr[maxIndex] < arr[i*2+1] ) {
-            maxIndex = i*2+1;
+        if(i*2+1<=heapSize&&arr[i*2+1]>arr[maxIndex]){
+            maxIndex=i*2+1
         }
-        if (maxIndex === i) break;
-        swap(arr, i, maxIndex); // 交换 
-        i = maxIndex; 
+        if(maxIndex===i)
+            break
+        swap(maxIndex,i,arr)
+        i=maxIndex    
     }
 }
 
 
-let swap = (arr, i , j) => {
-    if(i===j) return
-    let temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
-}
-
-let example=[,12, 5, 13, 14, 11]
-let result=heapSort(example)
-console.log(result)
+let example=[12, 5, 13, 14, 11]
+let res=heapSort(example)
+console.log(res)
