d81211ead90cf341d47f95d98ddb7cab763f59af
朱汉卿 <zhq199622@163.com>
2020-12-13 17:10:47 +0800

    Update 112. 路径总和.js

--- js/112. 路径总和.js
+++ js/112. 路径总和.js
@@ -22,20 +22,19 @@ var hasPathSum = function(root, sum) {
         if(hasPath){
             return
         }
-        if(root===null){
-            // 路径和满足 sum 且最后一个节点是叶子节点
-            if(preSum===sum&&ifLeaf){
-                hasPath=true
-            }
-            return
-        }
 
         // 需要判断当前节点是不是叶节点
         ifLeaf=!(root.left||root.right)
+
+        if(preSum+root.val===sum&&ifLeaf){
+            hasPath=true
+            return
+        }
+
             
         // 这里的思路和回溯很像，只是回溯需要for循环遍历，这里只要遍历 left 和 right 两个分支
-        traverse(root.left,preSum+root.val,ifLeaf)
-        traverse(root.right,preSum+root.val,ifLeaf)
+        root.left&&traverse(root.left,preSum+root.val,ifLeaf)
+        root.right&&traverse(root.right,preSum+root.val,ifLeaf)
     }
     traverse(root,0,(root.left||root.right))
     return hasPath
