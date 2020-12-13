ccc3a29d3e867b6b8ba0c23bb6d12e87f212c7cd
朱汉卿 <zhq199622@163.com>
2020-12-13 16:45:44 +0800

    Create 112. 路径总和.js

---
+++ js/112. 路径总和.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+
+// 左右
+var hasPathSum = function(root, sum) {
+    if(!root)
+        return false
+    let hasPath=false
+    const traverse=function(root,preSum,ifLeaf){
+        // 已经找到路径，提前结束查找
+        if(hasPath){
+            return
+        }
+        if(root===null){
+            // 路径和满足 sum 且最后一个节点是叶子节点
+            if(preSum===sum&&ifLeaf){
+                hasPath=true
+            }
+            return
+        }
+
+        // 需要判断当前节点是不是叶节点
+        ifLeaf=!(root.left||root.right)
+            
+        traverse(root.left,preSum+root.val,ifLeaf)
+        traverse(root.right,preSum+root.val,ifLeaf)
+    }
+    traverse(root,0,(root.left||root.right))
+    return hasPath
+};
