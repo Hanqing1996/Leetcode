d4c71cd7ca67c6046bd8d19add830b0e6604c76c
朱汉卿 <zhq199622@163.com>
2020-12-15 10:07:56 +0800

    Create 3. 无重复字符的最长子串.js

---
+++ js/3. 无重复字符的最长子串.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let queue=[]
+    let len=s.length
+    if(len===0)
+        return 0
+    let maxLen=1
+    for(let i=0;i<len;i++){
+
+        // 检测新元素是否已存在于 queue 中
+        let repeatIndex=queue.indexOf(s[i])
+        // 新元素是一定会被添加到 queue 中的
+        queue.push(s[i])
+        if(repeatIndex<0)
+            maxLen=Math.max(queue.length,maxLen)
+        else
+            queue=queue.slice(repeatIndex+1)
+    }
+    return maxLen
+};
