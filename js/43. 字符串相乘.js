aab0ba6934cf7ebbe8754696a7eb9e2b452869ff
朱汉卿 <zhq199622@163.com>
2020-12-16 09:03:21 +0800

    Update 43. 字符串相乘.js

--- js/43. 字符串相乘.js
+++ js/43. 字符串相乘.js
@@ -6,21 +6,17 @@
 var multiply = function(num1, num2) {
     let len1=num1.length
     let len2=num2.length
-    let index1,index2
-    
     let ans=[]
-
     for(let i=len1-1;i>=0;i--){
         for(let j=len2-1;j>=0;j--){
-            index1=i+j
-            index2=i+j+1
-            let mul=num1[i]*num2[j]
+            let index1=i+j
+            let index2=i+j+1
+            let mul=num1[i]*num2[j]+(ans[index2]|0)
             ans[index1]=Math.floor(mul/10)+(ans[index1]|0)
-            ans[index2]=(mul+(ans[index2]|0)) %10
+            ans[index2]= mul%10
+            
         }
     }
-    let result= ans.join('').replace(/^0+/,'')
-
-    // 处理 0*0
-    return !result ? '0' : result
+    let res=ans.join('').replace(/^0+/,'')
+    return !res?'0':res
 };
