957fd41e968cf59839b0a4ce70da1ab73eedef24
朱汉卿 <zhq199622@163.com>
2020-12-19 10:09:54 +0800

    Update 209. 长度最小的子数组【滑动窗口】.js

--- js/209. 长度最小的子数组【滑动窗口】.js
+++ js/209. 长度最小的子数组【滑动窗口】.js
@@ -12,25 +12,24 @@
  */
 
 // 
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
 var minSubArrayLen = function(s, nums) {
-    // 如果不存在符合条件的子数组，返回 0
-    let temp=nums.reduce((a,b)=>a+b,0)
-    if(temp<s)
-        return 0
-
-    let minLen=nums.length
-    let left=0,right=0
+    let queue=[]
     let sum=0
-    for(let right=0;right<nums.length;right++){
-        sum+=nums[right]
-        // 连续去掉 a 的若干项，可能 sum 依然大于等于 s,所以用 while
+    let minLen
+    for(let i=0;i<nums.length;i++){
+        queue.push(nums[i])
+        sum+=nums[i]
         while(sum>=s){
-            minLen=Math.min(minLen,right-left+1)
-            sum-=nums[left++]
+            minLen=minLen?Math.min(minLen,queue.length):queue.length
+            sum-=queue.shift()
         }
     }
-    return minLen
-    
+    return minLen?minLen:0
 };
 
 
