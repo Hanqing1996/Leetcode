a9e87af4f11e6f18b4094858a2159900bf677ddb
朱汉卿 <zhq199622@163.com>
2020-12-19 12:59:44 +0800

    Update 插入排序.js

--- js/排序算法/插入排序.js
+++ js/排序算法/插入排序.js
@@ -21,3 +21,39 @@ function insertionSort(arr){
 let arr=[5,3,1,9,2,8,7,10,6]
 let res=insertionSort(arr)
 console.log(res)
+
+
+// 优化：二分插入排序
+function swap(a,b,arr){
+    let temp=arr[a]
+    arr[a]=arr[b]
+    arr[b]=temp
+}
+
+function binaryInsertSort(arr){
+    let len=arr.length
+    
+    for(let i=1;i<arr.length;i++){
+
+        let target=arr[i]
+        let left=0,right=i
+
+        while(left<=right){
+            let mid=left+Math.floor((right-left)/2)
+            if(arr[mid]<target)
+                left=mid+1
+            else
+                right=mid-1      
+        }
+
+        for(let j=i;j>=left+1;j--){
+            arr[j]=arr[j-1]
+        }        
+        arr[left]=target
+    }
+    return arr
+}
+
+let arr=[5,3,1,9,2,8,7,10,6]
+let res=binaryInsertSort(arr)
+console.log(res)
