c5d675e0bf2b45c7954bc1d81f6bd652ce4cad19
朱汉卿 <zhq199622@163.com>
2020-12-20 09:39:16 +0800

    Update 插入排序.js

--- js/排序算法/插入排序.js
+++ js/排序算法/插入排序.js
@@ -8,7 +8,7 @@ function insertionSort(arr){
     let len=arr.length
     for(let i=1;i<arr.length;i++){
         // [...已经有序的数组,要放入有序数组的新元素]，对这个数组从后往前进行扫描，比较相邻两个元素（若后面的元素小于前面，则进行交换，否则停止扫描）
-        for(let j=i;j>=0;j--){
+        for(let j=i;j>=1;j--){
             if(arr[j]<arr[j-1])
                 swap(j,j-1,arr)
             else
