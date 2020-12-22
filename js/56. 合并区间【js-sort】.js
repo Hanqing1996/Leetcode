7b6778cec07158823a27f7861d2cd94ed515c6bd
朱汉卿 <zhq199622@163.com>
2020-12-22 09:44:27 +0800

    Update 56. 合并区间【js-sort】.js

--- js/56. 合并区间【js-sort】.js
+++ js/56. 合并区间【js-sort】.js
@@ -2,35 +2,29 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-
-/**
- * 参考：https://leetcode-cn.com/problems/merge-intervals/solution/chi-jing-ran-yi-yan-miao-dong-by-sweetiee/
- * 假设 a<c，则考虑 [a,b] 和 [c,d] 能不能合并，只需要看
- * 1.b<c：不能合并
- * 2.b>c：可以合并，合并后区间为[a,max(b,d)]  
- */
 var merge = function(intervals) {
-    // 排序，以保证 intervals 组成区间起点递增
-    intervals=intervals.sort((interval1,interval2)=>interval1[0]-interval2[0])
+    // 按 intervals[i][0] 递增排序
+    intervals=intervals.sort((a,b)=>a[0]-b[0])
 
-    let res=[]
-    let count=-1
-    for(let i=0;i<intervals.length;i++){
+    let len=intervals.length
+    let count=0
+    let res=[intervals[0]]
+    for(let i=1;i<len;i++){
         let current=intervals[i]
-        if(res.length===0){
-            res.push(current)
-            count++
-            continue
-        }
-        let last=res[count]
-        // current[0] 肯定不小于 last[0]
-        if(last[1]<current[0]){
-            res.push(current)
-            count++
-        }
+
+        /**
+         * 区间可合并有两种可能
+         * (1) [1,4],[2,5]=>[1,5]
+         * (2) [1,4],[2,3]=>[1,4]
+         * 即 
+         */
+
+        if(current[0]<=res[count][1])
+            res[count][1]=Math.max(res[count][1],current[1])
         else{
-            res[count]=[last[0],Math.max(last[1],current[1])]
-        }    
+            res.push(current)
+            count+=1
+        }
     }
     return res
 };
