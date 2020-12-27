9112789533cbfb5be6067c5c62814304796281dd
朱汉卿 <zhq199622@163.com>
2020-12-27 15:30:16 +0800

    Update 26. 删除排序数组中的重复项【数组遍历-双指针】.js

--- js/26. 删除排序数组中的重复项【数组遍历-双指针】.js
+++ js/26. 删除排序数组中的重复项【数组遍历-双指针】.js
@@ -4,17 +4,11 @@
  */
 
 
-// p 指向不重复的数，q用于遍历 nums 
 var removeDuplicates = function(nums) {
-    const {length:len}=nums
-    if(len===1) return 1 
     let p=0
-    for(let q=0;q<len;q++){
-        if(nums[q]!==nums[p]){
-            nums[p+1]=nums[q]
-            p++
-        }    
+    for(let q=1;q<nums.length;q++){
+        if(nums[q]!=nums[p])
+            nums[++p]=nums[q]
     }
-    // 因为能保证 p 每次指向的数字都不重复，因此当 q 遍历完整个 nums 数组时,p 已经指向最后一个不重复的数字
     return p+1
 };
