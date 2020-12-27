7f706fae864888096edde135066ae6bece678bca
朱汉卿 <zhq199622@163.com>
2020-12-27 12:51:49 +0800

    Update 415. 字符串相加.js

--- js/415. 字符串相加.js
+++ js/415. 字符串相加.js
@@ -4,16 +4,16 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
+    let carry=0
     let i=num1.length-1
     let j=num2.length-1
-    let carry=0
-    let ans=[]
-
+    let res=[]
     while(i>=0||j>=0||carry!=0){
-        i>=0&&(carry+=(num1[i--]-'0'))
-        j>=0&&(carry+=(num2[j--]-'0'))
-        ans.push(carry%10)
-        carry=Math.floor(carry/10)
+        let sum=0
+        i>=0&&(sum+=Number(num1[i--]))
+        j>=0&&(sum+=Number(num2[j--]))
+        res.unshift((sum+carry)%10)
+        carry=Math.floor((sum+carry)/10)
     }
-    return ans.reverse().join("") 
+    return res.join('')
 };
