79db5f85495f82c76fa9d87c70d85b33372eabc4
朱汉卿 <zhq199622@163.com>
2020-12-28 10:38:36 +0800

    Create 172. 阶乘后的零.js

---
+++ js/172. 阶乘后的零.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count=0
+    while(n>=5){
+        count+=Math.floor(n/5)
+        n=Math.floor(n/5)
+    }
+    return count
+};
