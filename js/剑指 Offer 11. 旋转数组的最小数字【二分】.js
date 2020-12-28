62353b220887825310deb86fcca6aed5c6b79ebd
朱汉卿 <zhq199622@163.com>
2020-12-28 11:09:12 +0800

    Update 剑指 Offer 11. 旋转数组的最小数字【二分】.js

--- js/剑指 Offer 11. 旋转数组的最小数字【二分】.js
+++ js/剑指 Offer 11. 旋转数组的最小数字【二分】.js
@@ -1,25 +1,19 @@
 /**
- * @param {number[]} numbers
+ * @param {number[]} nums
  * @return {number}
  */
 
-/**
- * numbers[high]<numbers[mid],[3,4,5,1,2]说明最小数肯定位于 mid 之后=>low=mid+1
- * numbers[high]>numbers[mid],[5,1,2,3,4]说明最小数只可能在 mid 及 mid 之前=>high=mid
- * numbers[high]=numbers[mid],[7,7,7,2,7]和[7,2,7,7,7]都有可能，只能说明最小数在 high 之前=>high=high-1
- */
-var minArray = function(numbers) {
-    let low=0,high=numbers.length-1,mid
-    while(low<=high){
-        mid=low+Math.floor((high-low)/2)
-        if(numbers[high]<numbers[mid])
-            low=mid+1
-        else if(numbers[high]>numbers[mid])
-            high=mid
-        else
-            high=high-1
+// 旋转数组，末尾元素一定小于等于第一个元素
+var findMin = function(nums) {
+    
+    let left=0,right=nums.length-1
+    while(left<right){
+        let mid=left+Math.floor((right-left)/2)
+        if(nums[mid]>nums[right])
+            left=mid+1
+        else 
+            right=mid
     }
-
-    // 因为题目保证最小的数存在，所以 while 结束时,一定是 low=high=mid
-    return numbers[mid]  
+    return nums[left] 
 };
+
