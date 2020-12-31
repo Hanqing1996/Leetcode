dbc47500bdbb7aa321a52297c130b5148ea7948d
朱汉卿 <zhq199622@163.com>
2020-12-31 10:00:41 +0800

    Update 53. 最大子序和.js

--- js/53. 最大子序和.js
+++ js/53. 最大子序和.js
@@ -1,15 +1,21 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * dp思路1:dp[i]表示以nums[i]结尾的连续子数组最大和
  */
 var maxSubArray = function(nums) {
-    // dp[i]表示以nums[i]结尾的最大连续子数组和
-    let dp=[]
-    dp[0]=nums[0]
-    maxSum=dp[0]
-    for(let i=1;i<nums.length;i++){
+    let len=nums.length
+    let dp=nums
+    if(len===0)
+        return 0
+    if(len===1)
+        return nums[0]
+    let res=dp[0]
+    // dp[0]=nums[0]
+    // dp[1]=max(dp[0]+nums[1],nums[1])
+    // ...
+    // dp[i]=max(dp[i-1]+nums[i],nums[i])
+    for(let i=1;i<len;i++){
         dp[i]=Math.max(dp[i-1]+nums[i],nums[i])
-        maxSum=Math.max(maxSum,dp[i])
+        res=Math.max(res,dp[i])
     }
-    return maxSum
+    return res
 };
