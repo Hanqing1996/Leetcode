ba811ea5602c2511891976016f72ca9cf03dd356
朱汉卿 <zhq199622@163.com>
2021-01-01 22:13:13 +0800

    Update 121. 买卖股票的最佳时机.js

--- js/121. 买卖股票的最佳时机.js
+++ js/121. 买卖股票的最佳时机.js
@@ -32,11 +32,37 @@ var maxProfit = function(prices) {
 
 
 /**
+ * dp思路1：dp[i]表示以nums[i]价格售出所能获得的最大利润
+ * 遍历prices,一路记录当前最小值,再对比nums[i]和最小值的差即可
+ */
+var maxProfit = function(prices) {
+    let len=prices.length
+    let dp=[]
+    dp[0]=0
+    let minPrice=prices[0]
+    let minIndex=0
+    let res=0;
+
+    for(let i=1;i<len;i++){
+        dp[i]=(prices[i]-minPrice)>0?(prices[i]-minPrice):0
+        res=Math.max(res,dp[i])
+        if(prices[i]<minPrice){
+            minIndex=i
+            minPrice=prices[i]
+        }
+    }
+    return res
+};
+
+
+
+
+/**
  * @param {number[]} prices
  * @return {number}
  */
 /**
- * 前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
+ * dp思路2:前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
  */
 var maxProfit = function(prices) {
     // dp[i] 表示前i天的最大收益
