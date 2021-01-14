21a78f99e39dc6430d9c1a81ce96036abf109ad5
朱汉卿 <zhq199622@163.com>
2021-01-14 09:57:33 +0800

    Update 647. 回文子串.js

--- js/647. 回文子串.js
+++ js/647. 回文子串.js
@@ -6,21 +6,23 @@
 /**
  * dp思路：dp[i][j]表示s[i]-s[j]是不是回文子串
  */
+// 由于采用 5. 最长回文子串 的遍历方式可查找到所有子串，判断它们是不是子串即可
 var countSubstrings = function(s) {
     let len=s.length
+
     let dp=Array.from({length:len},()=>Array.from({length:len},()=>false))
-    
-    let res=0
-    
-    for(let i=0;i<len;i++){
+
+    // 统计长度大于1的回文子串个数
+    let count=0
+    for(let i=1;i<len;i++){
         dp[i][i]=true
-        res++
         for(let j=0;j<i;j++){
-            if(s[i]===s[j]&&(dp[j+1][i-1]||j===i-1)){
+            if(s[j]===s[i]&&(dp[j+1][i-1]||j===i-1)){
                 dp[j][i]=true
-                res++
+                count++
             }
         }
     }
-    return res
+    // 长度大于1的回文子串个数+长度等于1的回文子串个数
+    return count+len
 };
