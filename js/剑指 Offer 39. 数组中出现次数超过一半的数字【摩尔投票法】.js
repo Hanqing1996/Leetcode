870366000d21a7b4385bb97e779cb4b08967d861
朱汉卿 <zhq199622@163.com>
2021-01-15 11:49:59 +0800

    Update 剑指 Offer 39. 数组中出现次数超过一半的数字【摩尔投票法】.js

--- js/剑指 Offer 39. 数组中出现次数超过一半的数字【摩尔投票法】.js
+++ js/剑指 Offer 39. 数组中出现次数超过一半的数字【摩尔投票法】.js
@@ -18,12 +18,25 @@ var majorityElement = function(nums) {
  * @param {number[]} nums
  * @return {number}
  */
+
+/**
+ * 战乱年间，各国各派出若干名武士，相约厮杀于将军台上。
+ * 约定每次向台中放入一名武士，对于前后两名武士，若隶属一国，则并肩而立，等待下名武士的到来。否则后者可与前者同归于尽（一拼一）。
+ * 毫无疑问，最后的胜利者，一定是派出武士最多的国家。
+ */
 var majorityElement = function(nums) {
-    let res=0,move=0
-    for(let i=0;i<nums.length;i++){
-        if(move===0)
-            res=nums[i]
-        move+=res===nums[i]?1:-1
+    let len=nums.length
+
+    let victor,currentNum=0
+
+    for(let i=0;i<len;i++){
+        if(currentNum===0){
+            victor=nums[i]
+        }
+        if(nums[i]===victor)
+            currentNum++
+        else
+            currentNum--
     }
-    return res
+    return victor
 };
