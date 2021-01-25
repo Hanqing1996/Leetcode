b9f681f209fb30ae32f879d927f7441189702cf4
朱汉卿 <zhq199622@163.com>
2021-01-25 10:46:48 +0800

    Update 55. 跳跃游戏.js

--- js/55. 跳跃游戏.js
+++ js/55. 跳跃游戏.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 
+// dp:O(n2) 
 // dp[i] 表示能否跳到第 i 个格子。最后返回 dp[num.length-1]
 var canJump = function(nums) {
     let len=nums.length;
@@ -18,4 +19,27 @@ var canJump = function(nums) {
         }
     }
     return dp[len-1]
+};
+
+
+// 贪心：O(n)
+// 贪心算法，每个位置都计算自己能达到的最远距离，同时每个位置要判断自己是否可达，也就是本位置需要在当前最远能到达的距离中。最终计算出来能到达的最远距离，与数组长度比较即可。
+var canJump = function(nums) {
+    let len=nums.length;
+    if(len<=1)
+        return true
+    let maxDistance=nums[0]
+    let res=false
+    for(let i=1;i<len;i++){
+        // 当前位置可达
+        if(i<=maxDistance){
+            maxDistance=Math.max(maxDistance,nums[i]+i)
+        }
+        if(maxDistance>=len-1){
+            res=true
+            break
+        }
+            
+    }
+    return res
 };
