0befa7de543176ae1d66d26bde45de8dc19165aa
朱汉卿 <zhq199622@163.com>
2021-02-12 13:26:35 +0800

    Create 34. 在排序数组中查找元素的第一个和最后一个位置.js

---
+++ js/34. 在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,25 @@
+var searchRange = function(nums, target) {
+    let left=0,right=nums.length-1
+    let res=[]
+
+    // 先找左边界
+    while(left<right){
+        let mid=left+Math.floor((right-left)/2)
+        if(nums[mid]>target) 
+            right=mid-1
+        else if(nums[mid]<target)
+            left=mid+1
+        else
+            right=mid
+    }
+    res[0]=left
+
+    // 再找右边界
+    let current=left
+    while(nums[current]===target&&current<nums.length)
+        current++
+    res[1]=current-1
+    return res
+};
+
+
