d1dd044f5fcb6ea44a01b8df2bd93ce5a820066f
朱汉卿 <zhq199622@163.com>
2021-02-14 22:30:03 +0800

    Update 226. 翻转二叉树.js

--- js/226. 翻转二叉树.js
+++ js/226. 翻转二叉树.js
@@ -1,34 +1,18 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree = function(root) {
+var buildTree = function(preorder, inorder) {
+  if(preorder.length===0)
+    return null
+  const traverse=function(currentIn){
+      const rootValue=preorder.shift()
+      let root=new TreeNode(rootValue)
 
-    if(root===null)
-        return root
-
-    const swap=function(root){
-        const temp=root.left
-        root.left=root.right
-        root.right=temp
-    }
-
-    const reverse=function(root){
-        let leftTree=root.left
-        let rightTree=root.right
-        if(leftTree!=null)
-            reverse(leftTree)
-        if(rightTree!=null)
-            reverse(rightTree)    
-        swap(root)
-    }
-    reverse(root)
-    return root
+      // 找到 preorder 首元素在 inorder 中的索引，从而划分出左右子树
+      const index=currentIn.indexOf(rootValue)
+      const leftPart=currentIn.slice(0,index)
+      const rightPart=currentIn.slice(index+1)
+      //
+      root.left=leftPart.length===0?null:traverse(leftPart)
+      root.right=rightPart.length===0?null:traverse(rightPart)
+      return root
+  }
+  return traverse(inorder)
 };
